6b506a54af495297277c4feee476d2f941ec5aea
qasim ali <muhammadqasimali717@gmail.com>
2024-12-09 02:30:38 +0500

    fix: error fix

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import bodyParser from 'body-parser';
 import chargebee from 'chargebee';
 import Stripe from 'stripe';
 
+
 import models, { connectDb } from './models';
 import routes from './routes';
 
@@ -15,12 +16,7 @@ chargebee.configure({
   api_key: process.env.CHARGEBEE_API_KEY,
 });
 
-app.use(
-  cors({
-    origin: ['http://localhost:3000', 'http://18.224.31.229', '*'],
-    credentials: true,
-  }),
-);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -30,7 +26,7 @@ app.use('/player', routes.player);
 app.use('/subscription', routes.subscription);
 
 connectDb().then(async () => {
-  app.listen(process.env.PORT, '0.0.0.0', () =>
+  app.listen(process.env.PORT, '0.0.0.0',  () =>
     console.log(`NBA App listening on port ${process.env.PORT}!`),
   );
 });
