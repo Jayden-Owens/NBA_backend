27be8761ecf105b9e3ca6bb82f559972e44ee3b8
Jayden Owens <77424127+Jayden-Owens@users.noreply.github.com>
2025-08-29 09:49:20 -0600

    add cors

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ const corsOptions = {
 };
 
 // // Middleware
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
